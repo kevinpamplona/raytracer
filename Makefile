ab62dac331b99756f99ebbdccc57bd8a1c667840
kevinpamplona <kevinpamplona@gmail.com>
2012-11-04 17:05:56 -0800

    added fully-functional FreeImage library and ran working test ouput color gradient;

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
 INCFLAGS = -I./glm-0.9.2.7 -I/usr/X11/include -I./include/
+LDFLAGS = -framework GLUT -framework OpenGL -L./osxlib/ \
+		-L"/System/Library/Frameworks/OpenGL.framework/Libraries" \
+		-lGL -lGLU -lm -lstdc++ -lfreeimage
+
 
 all: raytracer
 raytracer: main.o readfile.o Objects.o Objects.h readfile.h sphere.h tri.h trinormal.h variables.h vertexnormal.h Film.h Film.o
-	g++ $(INCFLAGS) -o raytracer main.o readfile.o Objects.o Film.o
+	g++ -o raytracer main.o readfile.o Objects.o Film.o $(INCFLAGS) $(LDFLAGS)
 main.o: main.cpp variables.h readfile.h Objects.h readfile.cpp
 	g++ $(INCFLAGS) -c main.cpp  
 readfile.o: readfile.cpp readfile.h
--- main.cpp
+++ main.cpp
@@ -3,9 +3,9 @@
 #include <fstream>
 #include <sstream>
 
+#include <FreeImage.h>
+
 #include "Objects.h"
-
-
 #include "readfile.h"
 #include "variables.h"
 
@@ -74,6 +74,35 @@ float specular[3];
 float shininess;
 float emission[3];
 
+void testPrint() {
+    
+    int width = 800;
+    int height = 600;
+    int BPP = 24;
+    
+    FIBITMAP* bitmap = FreeImage_Allocate(width,height,BPP);
+    RGBQUAD color;
+    
+    if (!bitmap) {
+        exit(1);
+    }
+    
+    for (int i=0; i<width; i++) {
+        for (int j=0; j<height; j++) {
+            color.rgbRed = 0;
+            color.rgbGreen = (double)i / width * 255.0;
+            color.rgbBlue = (double)j / height * 255.0;
+            
+            FreeImage_SetPixelColor(bitmap, i,j,&color);
+        }
+    }
+    
+    if (FreeImage_Save(FIF_PNG, bitmap, "test.png", 0)) {
+        cout << "Image successfully saved!\n";
+    } else {
+        cout << "Image not saved..";
+    }
+}
 
 void init() {
     cout << "Reading in scene file... \n";
@@ -92,12 +121,18 @@ void init() {
     
     cout << "An amount of " << spherecount << " spheres have been specified. \n";
     cout << "An amount of " << tricount << " triangles have been specified. \n";
-    cout << "An amount of " << trinormcount << " triangles with calculated vertex normals have been specified. \n";
+    cout << "An amount of " << trinormcount << " triangles with calculated vertex normals have been specified. \n\n";
 }
 
 
 int main (int argc, char * argv[]) {
+    FreeImage_Initialise();
     readFile(argv[1]);
     init();
+    
+    cout << "FreeImage " << FreeImage_GetVersion() << "\n";
+    cout << FreeImage_GetCopyrightMessage() << "\n\n";
+    testPrint();
+    FreeImage_DeInitialise();
     return 0;
 }
